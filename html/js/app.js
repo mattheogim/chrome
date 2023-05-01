73835010899d8be7c9e902f2976b3d13e1896602
Mattheo <mattheogim@gmail.com>
2023-05-01 14:33:29 -0700

    created html css and js
    learning how to use console.log and objects

---
+++ html/js/app.js
@@ -0,0 +1,77 @@
+// let a = 5;
+// let b = 2;
+// let myName = "matteo";
+// let veryLongName = "matteo";
+
+
+// myName = "Matteo";
+
+// console.log("your name is " + myName);
+
+
+// const amIHappy = true;
+
+// console.log("am I happy? " + amIHappy);
+
+// const amItired = null;
+
+// console.log("am I happy? " + amItired);
+
+
+// let something;
+// console.log("am I happy? " + something);
+
+// // arrays
+
+// const mon= "monday";
+// const tue= "tuesday";
+// const wed= "wednesday";
+// const thu= "thursday";
+// const fri= "friday";
+// const sat= "saturday";
+// const sun= "sunday";
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+
+
+// // get Item from array
+// console.log(daysOfWeek);
+
+// // add one item to array
+// daysOfWeek.push("newday");
+
+
+// console.log(daysOfWeek);
+// console.log(daysOfWeek[7]);
+
+
+// const playerName = "Matteo";
+// const playerPoints = 121212;
+// const playerHandsome = false;
+// const playerFat = "little bit";
+
+// player [0] == name
+// player [1] == points
+
+
+// const player = ["Matteo", 121212, false, "little bit"];
+
+
+const player = {
+    name: "Matteo",
+    points: 10,
+    fat: "true"
+};
+
+console.log(player);
+console.log(player.name);
+
+
+// update object
+
+console.log(player);
+player.fat = false;
+player.lastName = "Bergamini";
+player.points = player.points + 15;
+console.log(player);
