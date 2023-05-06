b50068b5f9186c4184b330023b9e60c1279ba478
Mattheo <mattheogim@gmail.com>
2023-05-06 03:01:28 -0700

    using toggle instead of long lines

--- html/js/app.js
+++ html/js/app.js
@@ -4,12 +4,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleH1Click() {
-    const clickedClass = "clicked";
-   if (h1.className === clickedClass) {
-      h1.className = "";
-   } else {
-      h1.className = clickedClass;
-   }
+   h1.classList.toggle("clicked");
 }
 h1.addEventListener("click", handleH1Click);
 
