69be5867856a5d6931aeca4e816345710928f730
Mattheo <mattheogim@gmail.com>
2023-05-06 01:28:12 -0700

    practicing the event listeners

--- html/js/app.js
+++ html/js/app.js
@@ -1,6 +1,24 @@
 const title = document.querySelector(".hello h1");
 
 
-title.innerText = "Got you!";
+console.dir(title);
 
+function handleTitleClick() {
+    title.style.color = "blue";
 
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.onclick = handleTitleClick;
+
+title.onmouseenter = handleMouseEnter;
+
+title.addEventListener("mouseleave", handleMouseLeave);
