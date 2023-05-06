e17cbcaec7eaeeb94f5f708008aaa0e72bd07a7b
Mattheo <mattheogim@gmail.com>
2023-05-06 04:07:45 -0700

    event-value-collect-no-refresh

--- html/js/app.js
+++ html/js/app.js
@@ -1,13 +1,13 @@
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleLoginBtnClick() {
-    const username = loginInput.value;
-    console.log(username);
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 
 }
 
 
+loginForm.addEventListener("submit", onLoginSubmit);
 
-loginButton.addEventListener("click", handleLoginBtnClick);
