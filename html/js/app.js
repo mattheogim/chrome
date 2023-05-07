2bc5bbe0bfccdbcb9c9ef7887de9d6a77892afe3
Mattheo <mattheogim@gmail.com>
2023-05-07 16:28:47 -0700

    adding class hidden
    created const and added new h1 line with hidden class.
    
    using JS function remove the hidden class @ h1 and add class on input so that it is visible when user enters the value.

--- html/js/app.js
+++ html/js/app.js
@@ -1,13 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-
+const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    loginForm.classList.add("hidden")
-    console.log(username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 
 }
 
